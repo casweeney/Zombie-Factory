6a230fb2f9e036bd359a08fec6b93dc7eb77e304
CodingCas <casweeno2000@gmail.com>
2022-03-31 22:44:55 +0100

    Completed Crypto Zombie game smart contract

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,40 +1,51 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.4.25;
 
-contract ZombieFactory {
+import "./ownable.sol";
+import "./safemath.sol";
 
-    //Step 7: declare our event here
-    event NewZombie(uint zombieId, string name, uint dna);
+contract ZombieFactory is Ownable {
 
-    // Step 1: Declare variables
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+  using SafeMath for uint256;
+  using SafeMath32 for uint32;
+  using SafeMath16 for uint16;
 
-    // Step 2: Create a Zombie struct with 2 properties
-    struct Zombie {
-        string name;
-        uint dna;
-    }
+  event NewZombie(uint zombieId, string name, uint dna);
 
-    // Step 3: Create an Array of zombies using the Zombie struct
-    Zombie[] public zombies;
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+  uint cooldownTime = 1 days;
 
-    // Step 4: Create a private function that creates zombies and populates the zombies array
-    function _createZombie(string memory _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        // Step 8: and fire event here
-        emit NewZombie(id, _name, _dna);
-    }
+  struct Zombie {
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
+  }
 
-    // Step 5: generate a random zombie dna
-    function _generateRandomDna(string memory _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
-    }
+  Zombie[] public zombies;
 
-    // Step 6: use the random dna to create a random zombie
-    function createRandomZombie(string memory _name) public {
-        uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
-    }
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
+
+  function _createZombie(string _name, uint _dna) internal {
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+    emit NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna(string _str) private view returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
+  }
+
+  function createRandomZombie(string _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+    uint randDna = _generateRandomDna(_name);
+    randDna = randDna - randDna % 100;
+    _createZombie(_name, randDna);
+  }
 
 }
